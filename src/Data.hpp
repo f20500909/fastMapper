0434d5a5009c20957f38fe943d3957a784f861cf
f20500909 <3213377511@qq.com>
2020-01-11 22:23:22 +0800

    2020 1   11

--- src/Data.hpp
+++ src/Data.hpp
@@ -8,134 +8,139 @@
 * Represent a 2D array.
 * The 2D array is stored in a single array, to improve cache usage.
 */
-template <typename T> class Data {
+template<typename T>
+class Data {
 
 public:
-	/**
-	* Height and width of the 2D array.
-	*/
-	unsigned height;
-	unsigned width;
+    /**
+    * Height and width of the 2D array.
+    */
+    unsigned height;
+    unsigned width;
 
-	/**
-	* The array containing the data of the 2D array.
-	*/
-	std::vector<T> data;
+    /**
+    * The array containing the data of the 2D array.
+    */
+    std::vector<T> data;
 
-	/**
-	* Build a 2D array given its height and width.
-	* All the array elements are initialized to default value.
-	*/
-	Data(unsigned height, unsigned width) noexcept
-		: height(height), width(width), data(width * height) {}
+    /**
+    * Build a 2D array given its height and width.
+    * All the array elements are initialized to default value.
+    */
+    Data() {
 
-	/**
-	* Build a 2D array given its height and width.
-	* All the array elements are initialized to value.
-	*/
-	Data(unsigned height, unsigned width, T value) noexcept
-		: height(height), width(width), data(width * height, value) {}
+    };
 
-	/**
-	* Return a const reference to the element in the i-th line and j-th column.
-	* i must be lower than height and j lower than width.
-	*/
-	const T &get(unsigned i, unsigned j) const noexcept {
-		assert(i < height && j < width);
-		return data[j + i * width];
-	}
+    Data(unsigned height, unsigned width) noexcept : height(height), width(width), data(width * height) {}
 
-	/**
-	* Return a reference to the element in the i-th line and j-th column.
-	* i must be lower than height and j lower than width.
-	*/
-	T &get(unsigned i, unsigned j) noexcept {
-		assert(i < height && j < width);
-		return data[j + i * width];
-	}
+    /**
+    * Build a 2D array given its height and width.
+    * All the array elements are initialized to value.
+    */
+    Data(unsigned height, unsigned width, T value) noexcept
+            : height(height), width(width), data(width * height, value) {}
 
-	/**
-	* Return the current 2D array reflected along the x axis.
-	*/
-	Data<T> reflected() const noexcept {
-		Data<T> result = Data<T>(width, height);
-		for (unsigned y = 0; y < height; y++) {
-			for (unsigned x = 0; x < width; x++) {
-				result.get(y, x) = get(y, width - 1 - x);
-			}
-		}
-		return result;
-	}
+    /**
+    * Return a const reference to the element in the i-th line and j-th column.
+    * i must be lower than height and j lower than width.
+    */
+    const T &get(unsigned i, unsigned j) const noexcept {
+        assert(i < height && j < width);
+        return data[j + i * width];
+    }
 
-	/**
-	* Return the current 2D array rotated 90° anticlockwise
-	*/
-	Data<T> rotated() const noexcept {
-		Data<T> result = Data<T>(width, height);
-		for (unsigned y = 0; y < width; y++) {
-			for (unsigned x = 0; x < height; x++) {
-				result.get(y, x) = get(x, width - 1 - y);
-			}
-		}
-		return result;
-	}
+    /**
+    * Return a reference to the element in the i-th line and j-th column.
+    * i must be lower than height and j lower than width.
+    */
+    T &get(unsigned i, unsigned j) noexcept {
+        assert(i < height && j < width);
+        return data[j + i * width];
+    }
 
-	/**
-	* Return the sub 2D array starting from (y,x) and with size (sub_width,
-	* sub_height). The current 2D array is considered toric for this operation.
-	*/
-	Data<T> get_sub_array(unsigned y, unsigned x, unsigned sub_width,
-		unsigned sub_height) const noexcept {
-		Data<T> sub_array_2d = Data<T>(sub_width, sub_height);
-		for (unsigned ki = 0; ki < sub_height; ki++) {
-			for (unsigned kj = 0; kj < sub_width; kj++) {
-				sub_array_2d.get(ki, kj) = get((y + ki) % height, (x + kj) % width);
-			}
-		}
-		return sub_array_2d;
-	}
+    /**
+    * Return the current 2D array reflected along the x axis.
+    */
+    Data<T> reflected() const noexcept {
+        Data<T> result = Data<T>(width, height);
+        for (unsigned y = 0; y < height; y++) {
+            for (unsigned x = 0; x < width; x++) {
+                result.get(y, x) = get(y, width - 1 - x);
+            }
+        }
+        return result;
+    }
 
-	/**
-	* Assign the matrix a to the current matrix.
-	*/
-	Data<T> &operator=(const Data<T> &a) noexcept {
-		height = a.height;
-		width = a.width;
-		data = a.data;
-		return *this;
-	}
+    /**
+    * Return the current 2D array rotated 90° anticlockwise
+    */
+    Data<T> rotated() const noexcept {
+        Data<T> result = Data<T>(width, height);
+        for (unsigned y = 0; y < width; y++) {
+            for (unsigned x = 0; x < height; x++) {
+                result.get(y, x) = get(x, width - 1 - y);
+            }
+        }
+        return result;
+    }
 
-	/**
-	* Check if two 2D arrays are equals.
-	*/
-	bool operator==(const Data<T> &a) const noexcept {
-		if (height != a.height || width != a.width) {
-			return false;
-		}
+    /**
+    * Return the sub 2D array starting from (y,x) and with size (sub_width,
+    * sub_height). The current 2D array is considered toric for this operation.
+    */
+    Data<T> get_sub_array(unsigned y, unsigned x, unsigned sub_width,
+                          unsigned sub_height) const noexcept {
+        Data<T> sub_array_2d = Data<T>(sub_width, sub_height);
+        for (unsigned ki = 0; ki < sub_height; ki++) {
+            for (unsigned kj = 0; kj < sub_width; kj++) {
+                sub_array_2d.get(ki, kj) = get((y + ki) % height, (x + kj) % width);
+            }
+        }
+        return sub_array_2d;
+    }
 
-		for (unsigned i = 0; i < data.size(); i++) {
-			if (a.data[i] != data[i]) {
-				return false;
-			}
-		}
-		return true;
-	}
+    /**
+    * Assign the matrix a to the current matrix.
+    */
+    Data<T> &operator=(const Data<T> &a) noexcept {
+        height = a.height;
+        width = a.width;
+        data = a.data;
+        return *this;
+    }
+
+    /**
+    * Check if two 2D arrays are equals.
+    */
+    bool operator==(const Data<T> &a) const noexcept {
+        if (height != a.height || width != a.width) {
+            return false;
+        }
+
+        for (unsigned i = 0; i < data.size(); i++) {
+            if (a.data[i] != data[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
 };
 
 /**
 * Hash function.
 */
 namespace std {
-	template <typename T> class hash<Data<T>> {
-	public:
-		size_t operator()(const Data<T> &a) const noexcept {
-			std::size_t seed = a.data.size();
-			for (const T &i : a.data) {
-				seed ^= hash<T>()(i) + (size_t)0x9e3779b9 + (seed << 6) + (seed >> 2);
-			}
-			return seed;
-		}
-	};
+    template<typename T>
+    class hash<Data<T>> {
+    public:
+        size_t operator()(const Data<T> &a) const noexcept {
+            std::size_t seed = a.data.size();
+            for (const T &i : a.data) {
+                seed ^= hash<T>()(i) + (size_t) 0x9e3779b9 + (seed << 6) + (seed >> 2);
+            }
+            return seed;
+        }
+    };
 } // namespace std
 
 #endif // FAST_WFC_UTILS_ARRAY2D_HPP_
--- src/color.hpp
+++ src/color.hpp
@@ -6,27 +6,47 @@
 /**
 * Represent a 24-bit rgb color.
 */
-struct Color {
-	unsigned char r, g, b;
+class Color {
+public:
+//	unsigned char r, g, b;
+    Color(){
+        data[0] = 0;
+        data[1] = 0;
+        data[2] = 0;
+    }
 
-	bool operator==(const Color &c) const noexcept {
-		return r == c.r && g == c.g && b == c.b;
-	}
+    Color(unsigned char r,unsigned char g,unsigned char b) {
+        data[0] = r;
+        data[1] = g;
+        data[2] = b;
+    }
 
-	bool operator!=(const Color &c) const noexcept { return !(c == *this); }
+    unsigned char data[3];
+
+    bool operator==(const Color &c) const noexcept {
+        for (int i = 0; i < 3; i++) {
+            if (data[i] != c.data[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    bool operator!=(const Color &c) const noexcept { return !(c == *this); }
 };
 
 /**
 * Hash function for color.
 */
 namespace std {
-	template <> class hash<Color> {
-	public:
-		size_t operator()(const Color &c) const {
-			return (size_t)c.r + (size_t)256 * (size_t)c.g +
-				(size_t)256 * (size_t)256 * (size_t)c.b;
-		}
-	};
+    template<>
+    class hash<Color> {
+    public:
+        size_t operator()(const Color &c) const {
+            return (size_t) c.data[0] + (size_t) 256 * (size_t) c.data[1] +
+                   (size_t) 256 * (size_t) 256 * (size_t) c.data[2];
+        }
+    };
 } // namespace std
 
 #endif // FAST_WFC_UTILS_COLOR_HPP_
